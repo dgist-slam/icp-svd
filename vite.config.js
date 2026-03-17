08074e1625b3ebfc3d8d4c3ca1ba867484aa0710
giseop kim <gsk@dgist.ac.kr>
2026-03-18 01:29:41 +0900

    fix: update base path to match renamed repository

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/icp-svd/',
+  base: '/icp-svd-mag4d-lidar-odometry/',
   server: {
     open: true,
   },
